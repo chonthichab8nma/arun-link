a65155cc5f045fd3282e4c24fdbb5466269f44c4
lisayaa07 <chanipron07112546@gmail.com>
2026-02-20 09:47:19 +0700

    ✨:edit partner

--- src/components/Partner.tsx
+++ src/components/Partner.tsx
@@ -76,11 +76,11 @@ export default function Customer() {
 
 
                   /* ===== Phone (sm) ===== */
-                  bottom-[-286px] right-[-1px]
+                  bottom-[-281.5px] right-[-1px]
                   h-[600px] sm:h-[600px] md:h-[720px]
 
                   /* ===== iPad (md) ===== */
-                  md:right-[-50px] md:bottom-[-138px] md:h-[460px]
+                  md:right-[-50px] md:bottom-[-186px] md:h-[460px]
 
                   /* ===== Laptop+ (lg) : ใช้ของเดิม ไม่เปลี่ยนดีไซน์ ===== */
                   lg:right-[-100px] lg:bottom-auto lg:-top-[203px] lg:h-[620px]
